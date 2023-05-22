9102df88b2f7332d8ad8cfd7d9a306f118ed0376
akthrms <akt.hrms@gmail.com>
2023-05-22 23:53:20 +0900

    feat: 実装

---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use serde::Deserialize;
+use std::{cmp::Ordering, collections::HashMap};
+
+const MAX_RANK_CNT: u64 = 10;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct EntryLog {
+    player_id: String,
+    handle_name: String,
+}
+
+#[allow(unused)]
+#[derive(Debug, Deserialize, Clone, PartialEq, Eq)]
+pub struct PlayLog {
+    create_timestamp: String,
+    player_id: String,
+    score: u64,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct GameResult {
+    rank: u64,
+    player_id: String,
+    handle_name: String,
+    score: u64,
+}
+
+impl GameResult {
+    pub fn joined_with_comma(self) -> String {
+        format!(
+            "{},{},{},{}",
+            self.rank, self.player_id, self.handle_name, self.score
+        )
+    }
+}
+
+pub fn read_entry_logs(filepath: &str) -> anyhow::Result<HashMap<String, EntryLog>> {
+    let mut entry_logs = HashMap::new();
+
+    let mut reader = csv::Reader::from_path(filepath)?;
+    for entry_log in reader.deserialize() {
+        let entry_log: EntryLog = entry_log?;
+        entry_logs.insert(entry_log.player_id.clone(), entry_log);
+    }
+
+    Ok(entry_logs)
+}
+
+pub fn read_play_logs(filepath: &str) -> anyhow::Result<Vec<PlayLog>> {
+    let mut play_logs = HashMap::new();
+
+    let mut reader = csv::Reader::from_path(filepath)?;
+    for play_log in reader.deserialize() {
+        let play_log: PlayLog = play_log?;
+        play_logs
+            .entry(play_log.player_id.clone())
+            .and_modify(|existing: &mut PlayLog| {
+                if existing.score < play_log.score {
+                    *existing = play_log.clone();
+                }
+            })
+            .or_insert_with(|| play_log);
+    }
+
+    Ok(play_logs
+        .into_iter()
+        .map(|(_, play_log)| play_log)
+        .collect())
+}
+
+pub fn sort_play_logs(play_logs: &mut [PlayLog]) {
+    play_logs.sort_by(|a, b| match b.score.cmp(&a.score) {
+        Ordering::Equal => a.player_id.cmp(&b.player_id),
+        other => other,
+    });
+}
+
+pub fn extract_game_result(
+    play_logs: Vec<PlayLog>,
+    entry_logs: HashMap<String, EntryLog>,
+) -> Vec<GameResult> {
+    let mut result = Vec::new();
+
+    let mut rank = 1;
+    let mut result_cnt = 1;
+    let mut prev_score = 0;
+
+    for play_log in play_logs {
+        if let Some(entry_log) = entry_logs.get(&play_log.player_id) {
+            if play_log.score < prev_score {
+                rank = result_cnt;
+            }
+
+            if MAX_RANK_CNT < rank {
+                break;
+            }
+
+            result.push(GameResult {
+                rank,
+                player_id: play_log.player_id,
+                handle_name: entry_log.handle_name.clone(),
+                score: play_log.score,
+            });
+            result_cnt += 1;
+            prev_score = play_log.score;
+        }
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{env, process};
+use yumemi_2023_rust::{extract_game_result, read_entry_logs, read_play_logs, sort_play_logs};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().skip(1).collect::<Vec<String>>();
+    if args.len() != 2 {
+        eprintln!("invalid argument number: expected 2, got {}", args.len());
+        process::exit(1);
+    }
+
+    let entry_logs = match read_entry_logs(&args[0]) {
+        Ok(entry_log) => entry_log,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+    let mut play_logs = match read_play_logs(&args[1]) {
+        Ok(play_log) => play_log,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+    sort_play_logs(&mut play_logs);
+
+    println!("rank,player_id,handle_name,score");
+    for result in extract_game_result(play_logs, entry_logs) {
+        println!("{}", result.joined_with_comma());
+    }
 }
